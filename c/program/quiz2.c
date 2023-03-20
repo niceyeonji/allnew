fa50906f2e2f9078cf8096a4012fd602b6ecc6a8
niceyeonji <ky827426@naver.com>
2023-03-20 14:28:41 +0900

    quiz

---
+++ c/program/quiz2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "libcheckprime.h"
+
+void main() {
+  while (1) {
+    int n;
+    printf("Input integer => ");
+    scanf("%d", &n);
+    if (n == 0) break;
+    if (checkprime(n) == n)
+      printf("%d is a prime number \n" ,n);
+    else
+      printf("%d is not prime number \n" ,n);
+  }
+}
