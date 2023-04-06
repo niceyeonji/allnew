1dd57dc458057a60abea780d0c4648fac83a748f
niceyeonji <niceyeonji@naver.com>
2023-04-06 11:07:05 +0900

    [MongoDB] Get Ip Address Automatically

--- mongodb/shard/config_shard.sh
+++ mongodb/shard/config_shard.sh
@@ -3,6 +3,9 @@
 # default mongodb daemon stop.
 systemctl stop mongod
 
+export IP_TEMP=$(ip addr | grep enp0s3 | grep inet | cut -d " " -f6 | cut -d "/" -f1 )
+echo $IP_TEMP
+
 # remove data directory
 if [ -d data ]; then
     rm -rf ./data
@@ -15,9 +18,7 @@ touch /shard/data/logs/configsvr.log
 
 mongod --config /shard/mongodConfig.conf &
 sleep 3s
-
-mongo 192.168.1.51:27019 < mongo_rs
-sleep 3s
+mongo $IP_TEMP:27019 < rs.init
 
 # router Server
 touch /shard/data/logs/mongorouter.log
@@ -30,18 +31,22 @@ mkdir -pv /shard/data/shard1db
 touch /shard/data/logs/shard1.log
 
 mongod --config /shard/mongodShard1.conf &
-sleep 1s
+sleep 2s
+mongo $IP_TEMP:27021 < rs.init
 
 # shard2 Server
 mkdir -pv /shard/data/shard2db
 touch /shard/data/logs/shard2.log
 
 mongod --config /shard/mongodShard2.conf &
-sleep 1s
+sleep 2s
+mongo $IP_TEMP:27022 < rs.init
 
 # process status
 ps -ef | grep mongo
 sleep 2s
+
+mongo $IP_TEMP:27017 < rs.addShard
 
 #netstatus
 netstat -ntlp
