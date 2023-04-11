7fc3007c0ac109d77408c617ad6918599465473e
niceyeonji <niceyeonji@naver.com>
2023-04-11 15:25:10 +0900

    mysql curd

--- node/auth/routes/main.js
+++ node/auth/routes/main.js
@@ -56,4 +56,31 @@ app.get('/selectQuery', (req, res) => {
   res.send(result);
 });
 
+// request 1, query 1
+app.post('/insert', (req, res) => {
+  const {id, pw} = req.body;
+  const result = connection.query('insert into user values (?, ?)', [id, pw]);
+  console.log(result);
+  res.redirect('selectQuery?userid=' + req.body.id);
+});
+
+// request 1, query 1
+app.post('/update', (req, res) => {
+  const {id, pw} = req.body;
+  const result = connection.query('update user set passwd=? where userid=?', [
+    pw,
+    id,
+  ]);
+  console.log(result);
+  res.redirect('selectQuery?userid=' + req.body.id);
+});
+
+// request 1, query 1
+app.post('/delete', (req, res) => {
+  const {id, pw} = req.body;
+  const result = connection.query('delete from user where userid=?', [id]);
+  console.log(result);
+  res.redirect('/select');
+});
+
 module.exports = app;
