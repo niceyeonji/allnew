b4c3d1156212135ebd5f2981fd4e8b680442bf8d
niceyeonji <niceyeonji@naver.com>
2023-05-23 21:18:36 +0900

    project2

--- python/project2/fastapi_temp.py
+++ python/project2/fastapi_temp.py
@@ -6,6 +6,8 @@ from bson.objectid import ObjectId
 import os.path
 import json
 import chardet
+import matplotlib.pyplot as plt
+
 
 pydantic.json.ENCODERS_BY_TYPE[ObjectId] = str
 
@@ -94,3 +96,40 @@ async def dea_data(monthly=None):
             return list(mycol.find().limit(10))
         else:
             return f"년월 = {monthly} 데이터가 존재하지 않습니다."
+
+@app.get("/temp_graph")
+def temp_graph(year1: int, year2: int):
+    plt.rcParams['font.family'] = "AppleGothic"
+
+    # JSON 파일로부터 데이터 읽어서 데이터프레임('df')으로 저장.
+    filename = 'temp_data.json'
+    df = pd.read_json(filename)
+
+    # '년월' 열을 날짜형으로 변환
+    df['년월'] = pd.to_datetime(df['년월'])
+
+    # 입력한 연도 데이터 필터링
+    df_year1 = df[df['년월'].dt.year == year1]
+    df_year2 = df[df['년월'].dt.year == year2]
+
+    # 그래프 그리기
+    plt.figure(figsize=(10, 6))
+    
+    # 입력한 연도 그래프 그리기
+    plt.plot(df_year1['년월'].dt.month, df_year1['평균기온(℃)'], marker='o', label=str(year1)+'년')
+    plt.plot(df_year2['년월'].dt.month, df_year2['평균기온(℃)'], marker='o', label=str(year2)+'년')
+
+    plt.xlabel('월')
+    plt.ylabel('평균기온(℃)')
+    plt.title(str(year1)+'년 vs. '+str(year2)+'년 월별 평균기온')
+    plt.legend()
+
+    # x축 눈금 설정
+    plt.xticks(range(1, 13), ['01월', '02월', '03월', '04월', '05월', '06월', '07월', '08월', '09월', '10월', '11월', '12월'])
+
+
+    filename = 'tempGraph_'+str(year1)+'_'+str(year2)+'.png'
+    plt.savefig(filename, dpi=400, bbox_inches='tight')
+    plt.close()
+
+    return {"message": "그래프가 생성되었습니다.", "filename": filename}
