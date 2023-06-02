7c1a928891f7dca79755a4b6db9e6b5f305d6fab
niceyeonji <niceyeonji@naver.com>
2023-06-02 16:00:59 +0900

    project2

--- python/project2/meatjson.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import json
-
-csv_data = pd.read_csv("meat.csv", sep = ",")
-csv_data.to_json("meat.json", orient = "records")
-# json_data = json.dumps({"meat": csv_data.to_dict(orient="records")})
-
-# with open("meat.json", "w", encoding="utf-8") as json_file:
-#     json_file.write(json_data)
